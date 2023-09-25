7915c5bc12b7c81a184ebd40297fcb97af43d1e9
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2023-09-25 21:42:27 +0700

    Update info.py

--- src/info.py
+++ src/info.py
@@ -10,14 +10,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         parse_mode="markdown",
-        text="Hello!!! This is CypherCAT from CypherBOT \n"
-        "I can do a copy trade from influencer/wallet, snipe tokens, buy presales, place limit orders, swap tokens and many more \n"
+        text="Hi, This is CypherCAT from CypherBOT\n"
+        "I can do a copy trade from influencer/wallet, snipe tokens, buy presales, place limit orders, swap tokens and many more\n"
         "You can also ask me for the current price of any crypto by typing:\n\n"
-        "`/p <crypto_ticker>` or /cmc <crpto_ticker> \n\n"
-        "For example, `/p btc` \n"
-        "will give you the current price of Bitcoin. \n\n"
-        "To display the complete list of price, type `/cmc <crypto_ticker>`. \n"
-        "Type /help to see all available command, some feature still on developing \n\n"
+        "`/p <crypto_ticker>` or /cmc <crpto_ticker>\n\n"
+        "For example, `/p btc`\n"
+        "will give you the current price of Bitcoin.\n\n"
+        "To display the complete list of price, type `/cmc <crypto_ticker>`.\n"
+        "Type `/help` to see all available command, some feature still on developing\n\n"
         "By proceeding to use the bot, you confirm that you have read and agreed to our [Terms of Service](https://cypherbot.tech)",
     )
     logging.info("Start call")
@@ -38,8 +38,9 @@ async def bot_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/gas - to receive real time gas information on ERC\n"
         "/news - to receive CoinTelegraph news\n"
         "/help - to receive this message\n\n"
-        "This bot is written with love by CypherBOT Team"
-        "[Dev](https://t.me/asinpeda) - [Website](https://cypherbot.tech)\n\n"
+        "This bot is written with love by CypherBOT Team\n"
+        "[Dev](https://t.me/asinpeda)\n"
+        "[Website](https://cypherbot.tech)\n\n"
         "`Bot version: LucyHarun v1.0.1`\n",
     )
     logging.info(f"Help call")
