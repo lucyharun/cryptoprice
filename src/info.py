add56ce10dba0f2c7f67ab570631146530fd88bd
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2023-09-25 20:09:45 +0700

    Update info.py

--- src/info.py
+++ src/info.py
@@ -10,10 +10,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         parse_mode="markdown",
-        text="Hi! I am HCryptoPrice, You can ask me for the current price of any crypto by typing:\n\n"
-        "`/p <crypto_symbol>` \n\n"
+        text="Did you know, you can ask me for the current price of any crypto by typing:\n\n"
+        "`/p <crypto_ticker>` \n\n"
         "For example, `/p btc` will give you the current price of Bitcoin. Enjoy!\n\n"
-        "To display the complete list of commands, type `/help`",
+        "To display the complete list of price, type `/cmc <crypto_ticker>`"
+        "Type `/help` to see all available command, some feature still on developing",
     )
     logging.info("Start call")
 
@@ -23,7 +24,7 @@ async def bot_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id,
         parse_mode="markdown",
         disable_web_page_preview=True,
-        text="📚*List of Commands:*\n\n"
+        text="*List of Commands:*\n\n"
         "`/p <crypto_symbol>` - to receive the current price and historical variation from CoinCecko\n"
         "`/c <crypto_symbol>` - to receive the chart in different timeframes of selected crypto\n"
         "`/cmc <crypto_symbol>` - to receive the current price and historical variation from CoinMarketCap\n"
@@ -33,8 +34,8 @@ async def bot_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/gas - to receive real time gas information on ERC\n"
         "/news - to receive CoinTelegraph news\n"
         "/help - to receive this message\n\n"
-        "This bot is written with open-source and free code, and you can find it all at "
-        "[GitHub](https://github.com/halon176/h-crypto-price-bot)\n\n"
-        "`Bot version: 1.1.0`\n",
+        "This bot is written with love by CypherBOT Team"
+        "[Dev](https://t.me/asinpeda)\n\n"
+        "`Bot version: 1.0.1`\n",
     )
     logging.info(f"Help call")
